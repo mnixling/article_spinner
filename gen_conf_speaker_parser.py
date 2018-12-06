0066048e50d1f555925bf5445a93e8054f48ba3b
Michael Nix <42980689+mnixling@users.noreply.github.com>
2018-12-06 11:31:21 -0700

    Update gen_conf_speaker_parser.py

--- gen_conf_speaker_parser.py
+++ gen_conf_speaker_parser.py
@@ -1,21 +1,50 @@
+"""This script parses all the html code and extracts all the text."""
+
+
 import os
 import glob
 from bs4 import BeautifulSoup as BS
 
-"""
-1. Glob each speaker's individual folder's content into a list
-2. Iterate over the list to extract the talk
-3. Save the talk in the source_data folder as <speaker>_talk_0<(#)#>.txt
-"""
+speakers = ['russell-m-nelson', 'dallin-h-oaks', 'henry-b-eyring',
+            'm-russell-ballard', 'jeffrey-r-holland', 'dieter-f-uchtdorf',
+            'david-a-bednar', 'quentin-l-cook', 'd-todd-christofferson',
+            'neil-l-andersen', 'ronald-a-rasband', 'gary-e-stevenson',
+            'dale-g-renlund', 'gerrit-w-gong', 'ulisses-soares',
+            'angel-abrea', 'reyna-i-aburto', 'marcos-a-aidukaitis',
+            'silvia-h-allred', 'jose-l-alonso', 'carlos-h-amado',
+            'wilford-w-andersen', 'koichi-aoyagi', 'ian-s-ardern',
+            'mervyn-b-arnold', 'carlos-e-asay', 'brian-k-ashton',
+            'marvin-j-ashton', 'eduardo-ayala', 'robert-l-backman',
+            'steven-r-bangerter', 'ben-b-banks', 'w-mark-bassett',
+            'merrill-j-bateman', 'david-s-baxter', 'david-l-beck',
+            'julie-b-beck', 'randall-k-bennett', 'ezra-taft-benson',
+            'jean-b-bingham', 'shayne-m-bowen', 'william-r-bradford',
+            'mark-a-bragg', 'ted-e-brewerton', 'm-joseph-brough',
+            'monte-j-brough', 'l-edward-brown', 'dean-r-burgess',
+            'h-david-burton', 'linda-k-burton', 'f-enzio-busche',
+            'douglas-l-callister', 'tad-r-callister', 'helio-r-camargo',
+            'george-i-cannon', 'robert-w-cantwell', 'craig-a-cardon',
+            'bruce-a-carlson', 'matthew-l-carpenter', 'gerald-j-causse',
+            'sheldon-f-child', 'yoon-h-choi', 'albert-choules-jr',
+            'craig-o-christensen', 'shirley-d-christensen',
+            'darwin-b-christenson', 'kim-b-clark', 'don-r-clark',
+            'j-richard-clarke', 'l-whitney-clayton', 'weatherford-t-clayton',
+            'gayle-m-clegg', 'gary-j-coleman', 'spencer-j-condie',
+            'carl-b-cook', 'gene-r-cook', 'mary-n-cook', 'richard-e-cook',
+            'lawrence-e-corbridge', 'bonnie-h-cordon', 'valeri-v-cordon',
+            'j-devn-cornish', 'claudio-r-m-costa', 'joaquin-e-costa',
+            'michelle-d-craig', 'rulon-g-craven', 'legrand-r-curtis',
+            'derek-a-cuthbert', 'charles-w-dahlquist-ii', 'elaine-s-dalton',
+            'adhemar-damiani', 'dean-myron-davies', 'massimo-de-feo',
+            'benjamin-de-hoyos', 'robert-k-dellenbach', 'royden-g-derrick',
+            'gordon-b-hinckley', 'thomas-s-monson', 'james-e-faust',
+            'richard-g-scott']
 
-
-speakers = ['russell-m-nelson', 'dallin-h-oaks']
-
+"""Globs all the files and places them into a list"""
 def glob_file():
     glob_file_list = []
     for i in speakers:
-        partial_path = r'nix_semester_project\100_speaker_data\{}\source_data'.format(i)
-
+        partial_path = r'data\100_speaker_data\{}\source_data'.format(i)
         for filename in sorted(glob.glob(os.path.join(partial_path, '*.html'))):
             glob_file_list.append(filename)
     return glob_file_list
@@ -26,12 +55,10 @@ for filename in glob_file():
         html_code = input_file.read()
         html_soup = BS(html_code, 'html.parser')
 
-
-        with open(filename.replace('html', '.txt'), 'w', encoding='utf-8') as txt_file:
+        with open(filename.replace('.html', '.txt'), 'w', encoding='utf-8') as txt_file:
             if html_soup is not None:
                 for talk_title in html_soup.find_all('html'):
                     print(talk_title.title.string, file=txt_file)
                     for p in html_soup.find_all('p'):
                         text = p.get_text()
                         print(text, file=txt_file)
-
