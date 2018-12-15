85316b42ec350331679f647545ee7b80ee8f4ee4
Michael Nix <42980689+mnixling@users.noreply.github.com>
2018-12-15 10:42:51 -0700

    Update ML_features.py

--- ML_features.py
+++ ML_features.py
@@ -1,9 +1,9 @@
 import os
 import glob
-import re
-from string import punctuation as punct
+
 
 import nltk
+from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
 
 speakers = ['russell-m-nelson', 'dallin-h-oaks', 'henry-b-eyring',
@@ -41,6 +41,7 @@ speakers = ['russell-m-nelson', 'dallin-h-oaks', 'henry-b-eyring',
             'gordon-b-hinckley', 'thomas-s-monson', 'james-e-faust',
             'richard-g-scott']
 
+
 def all_files():
     all_files_list = []
     spun_data_rel_path = r'data\spun_data'
@@ -55,6 +56,7 @@ def all_files():
         all_files_list.append(file_name)
     return all_files_list
 
+
 def register(filename):
     for i in speakers:
         if i in filename:
@@ -63,24 +65,69 @@ def register(filename):
             return 'Fake'
 
 
-
-
 def ttr(in_Text):
     return len(set(in_Text)) / len(in_Text)
 
 
-feature_names = ['ttr', 'REGISTER']
+def sent_len_ratio(in_Text):
+    return len(sent_text) / len(in_Text)
+
+
+def prop_noun_ratio(tag_text):
+    prop_noun_count = len([i for i in tag_text if i[1].startswith('NNP')])
+    return prop_noun_count / len(tag_text)
+
+
+def modal_ratio(tag_text):
+    """Compute the modal-token ratio for input Text"""
+    modal_count = len([i for i in tag_text if i[1].startswith('MD')])
+    return modal_count / len(tag_text)
+
+
+def det_ratio(tag_text):
+    det_count = len([i for i in tag_text if i[1].startswith('DT')])
+    return det_count / len(tag_text)
+
+
+def adj_ratio(tag_text):
+    adj_count = len([i for i in tag_text if i[1].startswith('JJ')])
+    return adj_count / len(tag_text)
+
+
+def avg_word_len(in_Text):
+    average = sum(len(word) for word in in_Text) / len(tok_text)
+    return average
+
+
+def stop_word_ratio(in_Text):
+    stop_count = len([i for i in stopwords.words('english') if i in in_Text])
+    return stop_count / len(in_Text)
+
+
+feature_names = ['ttr', 'sent_len_ratio', 'prop_noun_ratio', 'modal_ratio',
+                 'det_ratio', 'adj_ratio', 'stop_ratio', 'avg_word_len',
+                 'REGISTER']
 with open('mc_feat_names.txt', 'w') as name_file:
     name_file.write('\t'.join(feature_names))
+
+"""
+It is important to have the feature names and the associated function in the 
+same order. The 'REGISTER' and register() must be the last elements in their 
+respective series. Do not change the name of 'REGISTER' else the ML_evaluation.py
+script will not work properly.
+"""
 
 with open('mc_features.csv', 'w') as output_file:
     for file in all_files():
         print('.', end='', flush=True)
         with open(file, encoding='utf-8') as current_file:
             text = current_file.read()
-        # sent_text = sent_tokenize(text)    THIS MAY BE USED LATER???????
+        sent_text = sent_tokenize(text)
         tok_text = nltk.word_tokenize(text)
-        # tag_text = nltk.pos_tag(tok_text)  THIS MAY BE USED LATER???????
-        print(ttr(tok_text), register(file), sep=',', file=output_file)
+        tag_text = nltk.pos_tag(tok_text)
+        print(ttr(tok_text), sent_len_ratio(text), prop_noun_ratio(tag_text),
+              modal_ratio(tag_text), det_ratio(tag_text), adj_ratio(tag_text),
+              stop_word_ratio(tok_text), avg_word_len(text),
+              register(file),
+              sep=',', file=output_file)
 
-    print()
